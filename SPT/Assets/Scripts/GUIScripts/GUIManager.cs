8abc75b9195f41781c8e19a6c9cc7ef5e7be2e94
Royal-Pumpkin <39552524+Royal-Pumpkin@users.noreply.github.com>
2018-06-04 17:18:36 +0900

    Revert "Merge branch 'master' of https://github.com/Royal-Pumpkin/UnityProject"
    This reverts commit adbcb8cb2ccf1d54d3d4fef464fc1c34f9326a43, reversing
    changes made to 244ced71cbd8be4571c52212e8024659028de637.

--- SPT/Assets/Scripts/GUIScripts/GUIManager.cs
+++ SPT/Assets/Scripts/GUIScripts/GUIManager.cs
@@ -4,45 +4,22 @@ using UnityEngine;
 
 public class GUIManager : MonoBehaviour {
     public List<GameObject> mListGUIScene = new List<GameObject>();
-    public GameObject mMainGUI;
-    public enum eGUISceneName {NULL=-1,PLAYSCENE,CONTROLSCENE,MAIN, ARMORY,STAGE }
+    public enum eGUISceneName {NULL=-1,PLAYSCENE,CONTROLSCENE }
     eGUISceneName mGUISceneState;
     public GUIControlMode mGUIControlMode;
     public GUINomalMode mGUINomalMode;
 
 	// Use this for initialization
 	void Start () {
+        mGUISceneState = eGUISceneName.PLAYSCENE;
 
-        SetGUIScene(eGUISceneName.MAIN);
     }
 	
 	// Update is called once per frame
 	void Update () {
 
         //나중에 스택으로 바꾸던지 해서 업데이트에서 관리안하고 바뀔때만 부르는 함수로 만들기
-		//for(int i=0; i<mListGUIScene.Count;i++)
-  //      {
-  //          if ((int)mGUISceneState == i)
-  //          {
-  //              mListGUIScene[i].SetActive(true);
-  //          }
-  //          else
-  //              mListGUIScene[i].SetActive(false);
-  //      }
-	}
-
-    public void SetGUIScene(eGUISceneName _GUISceneName)
-    {
-        mGUISceneState = _GUISceneName;
-        if(_GUISceneName== eGUISceneName.PLAYSCENE|| _GUISceneName == eGUISceneName.CONTROLSCENE)
-        {
-            mMainGUI.SetActive(false);
-        }
-        else
-        {
-            mMainGUI.SetActive(true);
-        }
-        for (int i = 0; i < mListGUIScene.Count; i++)
+		for(int i=0; i<mListGUIScene.Count;i++)
         {
             if ((int)mGUISceneState == i)
             {
@@ -51,11 +28,10 @@ public class GUIManager : MonoBehaviour {
             else
                 mListGUIScene[i].SetActive(false);
         }
+	}
 
-    }
-
-    public void SetGUIScene(int _GUISceneName)
+    public void SetGUIScene(eGUISceneName _GUISceneName)
     {
-        SetGUIScene((eGUISceneName)_GUISceneName);
+        mGUISceneState = _GUISceneName;
     }
 }
--- SPT/Assets/Scripts/GameManager.cs
+++ SPT/Assets/Scripts/GameManager.cs
@@ -8,11 +8,12 @@ public class GameManager : MonoBehaviour
     static public GameManager stGameManager;
     public EventManger mEventManager;
     public GUIManager mGUIManager;
-    public Player player;
+
     //관리 오브젝트 리스트
     List<GameObject> listFieldEnemy = new List<GameObject>();
     public int nListFieldidx = 0;
     List<Tower> listTower = new List<Tower>();
+
 
     //현재 게임상태
     public enum eGameState { NULL=-1,PLAY,PAUSE,GAMEOVER}
@@ -66,19 +67,17 @@ public class GameManager : MonoBehaviour
             listFieldEnemy.Add(tempobj);
             
             tempobj.SetActive(false);
-        }        
-    }
+        }
 
-    public void StageStart()
-    {
+
         StartCoroutine("SpawnCorutine");
         StartCoroutine("InGameCorutine");
 
         vecDefaultPos = Camera.main.transform.position;
         quaDefaultPos = Camera.main.transform.rotation;
         mGUIManager.mGUIControlMode.FuncctionSet();
-        mGUIManager.SetGUIScene(GUIManager.eGUISceneName.PLAYSCENE);
     }
+
     //private void Update()
     //{
         
@@ -192,12 +191,6 @@ public class GameManager : MonoBehaviour
 
             yield return new WaitForFixedUpdate();
         }
-    }
-
-    public void ChangeGold(int value)
-    {
-        int gold = player.ChangeGold(value);
-
     }
 
 }
