a673aafee19ccb7ad7ae9483d94af23833514671
necroria <bjy8256@hanmail.net>
2018-06-04 16:55:51 +0900

    Revert "스테이지 전환 추가"
    This reverts commit 4ee3badae52f7fa9e88afca86736acc8c376a0bd.

--- SPT/Assets/Scripts/GameManager.cs
+++ SPT/Assets/Scripts/GameManager.cs
@@ -56,19 +56,17 @@ public class GameManager : MonoBehaviour
             listFieldEnemy.Add(tempobj);
             
             tempobj.SetActive(false);
-        }        
-    }
+        }
 
-    public void StageStart()
-    {
+
         StartCoroutine("SpawnCorutine");
         StartCoroutine("InGameCorutine");
 
         vecDefaultPos = Camera.main.transform.position;
         quaDefaultPos = Camera.main.transform.rotation;
         mGUIManager.mGUIControlMode.FuncctionSet();
-        mGUIManager.SetGUIScene(GUIManager.eGUISceneName.PLAYSCENE);
     }
+
     //private void Update()
     //{
         
